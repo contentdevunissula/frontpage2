a49bf908aa70fc9325e98395b1d0585b84bde65b
= <=>
2021-08-20 13:05:08 +0700

    decode token for check user have class

--- src/constants/api/courses.js
+++ src/constants/api/courses.js
@@ -1,10 +1,9 @@
 import axios from "../../../src/configs/axios";
 import Cookies from 'js-cookie'
 
-const token = Cookies.get('TokenAuthorization') != undefined ? Cookies.get('TokenAuthorization') : ""
-
+const user_id = Cookies.get('id') !== undefined ? Cookies.get('id') : ""
 export default {
   all: (options = { params: { status: "published" } }) =>
     axios.get(`/courses`, options).then((res) => res.data),
-  details: (id, options = {headers : { Authorization : token }}) => axios.get(`/courses/${id}`, options).then((res) => res.data),
+  details: (id, options = {headers : { Authorization : user_id }}) => axios.get(`/courses/${id}`, options).then((res) => res.data),
 };
